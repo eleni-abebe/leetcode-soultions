57a8b9208f1c2d937650fccace5f6850782235a2
eleni-abebe <165511130+eleni-abebe@users.noreply.github.com>
2024-08-13 13:04:40 -0700

    Time: 52 ms (73.47%) | Memory: 11.9 MB (73.40%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -4,11 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        
+        output = nums*2
+        return output
+        
+
+
+        """
         ans=[]
         while len(ans)<2*len(nums):
             for i in nums:
                 ans.append(i)
                 
         return ans
-       
+       """
         
